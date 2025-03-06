2f733d6f6837beb642af4d2733b64dfa89f33f0c
Delfina Echarren <delfinaecharren@gmail.com>
2025-03-06 19:05:33 -0300

    ahora si se leen los posts

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 
 class Post(models.Model):
-    titulo = models.CharField(max_length=200)
+    titulo = models.CharField(max_length=100)
     contenido = models.TextField()
     fecha_publicacion = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return self.titulo
-# Create your models here.
+    imagen = models.ImageField(upload_to='posts/', blank=True, null=True)
--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,9 @@ from django.shortcuts import render, redirect
 from .models import Post
 from .forms import PostForm
 
-# Vista para listar los posts
 def lista_posts(request):
-    posts = Post.objects.all().order_by('fecha_publicacion')  # Obtiene todos los posts, ordenados por fecha descendente
-    resena = "Bienvenid@ a mi blog. Mi nombre es Delfina, soy ingeniera quimica de Santa Fe, Argentina. Gracias por leer :)."
-    return render(request, 'blog/lista_posts.html', {'posts': posts, 'resena': resena})
+    posts = Post.objects.all()  
+    return render(request, 'blog/lista_posts.html', {'posts': posts})
 
 # Vista para crear un nuevo post
 def crear_post(request):
